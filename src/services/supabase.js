032c77d7a53755f63678c335518442a8d6285139
Pezhwa <pejhvaman@gmail.com>
2025-02-25 12:58:49 +0330

    Added: supabase services and tables created

---
+++ src/services/supabase.js
@@ -0,0 +1,10 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://gugxdslltzahypbcdali.supabase.co";
+
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd1Z3hkc2xsdHphaHlwYmNkYWxpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA0NjUxOTEsImV4cCI6MjA1NjA0MTE5MX0.tAHKtJHnpTxkGorQ28LPF9UBfuQnw1n7b8GBRbTY_GA";
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
