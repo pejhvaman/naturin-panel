482b4ee670626029148fc3cd1d57fb398f52c637
Pezhwa <pejhvaman@gmail.com>
2025-03-02 11:06:01 +0330

    Abstracted: getting cabins to its hook

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,8 +1,7 @@
-import { useQuery } from "@tanstack/react-query";
-import { getCabins } from "../../services/apiCabins";
 import styled from "styled-components";
 import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
+import { useCabins } from "./useCabins";
 
 const Table = styled.div`
   border: 1px solid var(--color-grey-200);
@@ -30,12 +29,9 @@ const TableHeader = styled.header`
 `;
 
 function CabinTable() {
-  const { data: cabins, isLoading } = useQuery({
-    queryKey: ["cabins"],
-    queryFn: getCabins,
-  });
+  const { isCabinsLoading, cabins } = useCabins();
 
-  if (isLoading) return <Spinner />;
+  if (isCabinsLoading) return <Spinner />;
 
   return (
     <Table role="table">
