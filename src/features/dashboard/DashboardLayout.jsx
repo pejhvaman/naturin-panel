73ebcd21a341cfd36ca192cd7a27c821a7bcf39a
pejhvaman <pejhvaman@gmail.com>
2025-04-05 11:07:25 +0330

    Added: dashboard layout and computing recent bookings and stays

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import useRecentBookings from "./useRecentBookings";
+import useRecentStays from "./useRecentStays";
+import Spinner from "../../ui/Spinner";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +9,24 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  const { bookings, isLoading: isLoading1 } = useRecentBookings();
+  const { stays, confirmedStays, isLoading: isLoading2 } = useRecentStays();
+
+  if (isLoading1 || isLoading2) return <Spinner />;
+  console.log("bookings:", bookings);
+  console.log("stays:", stays);
+  console.log("CStays:", confirmedStays);
+
+  return (
+    <StyledDashboardLayout>
+      <div>statistics</div>
+      <div>today&apos;s staty</div>
+      <div>chart stay duration</div>
+      <div>chart sales</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
